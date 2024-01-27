ad3a5ef9c6ba46331a029bf7c13149a8115bca6c
Wictor, Monteiro <104950871+wictor-monteiro@users.noreply.github.com>
2024-01-27 03:34:39 -0300

    Create words.js

---
+++ js/words.js
@@ -0,0 +1,26 @@
+let words = [
+    {
+        word: "love",
+        hint: "A feeling for someone or persons"
+    },
+    {
+        word: "color",
+        hint: "Way of being, the ..."
+    },
+    {
+        word: "girl",
+        hint: "Female person"
+    },
+    {
+        word: "lion",
+        hint: "The king of jungle"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+]
